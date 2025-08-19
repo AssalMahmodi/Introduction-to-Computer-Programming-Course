6870a2e8ee5b3e58176f2650f20930b455addf98
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 10:54:42 +0330

    Update 03_Fibonacci.cpp

--- 03_Fibonacci.cpp
+++ 03_Fibonacci.cpp
@@ -1,1 +1,14 @@
-
+long long fibonacci() {
+    int n;
+    cout << "Enter n for Fibonacci: ";
+    cin >> n;
+    if (n <= 1) return n;
+    long long a = 0, b = 1, c;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    cout << "Fibonacci number " << n << " is " << b << endl;
+    return b;
+}
