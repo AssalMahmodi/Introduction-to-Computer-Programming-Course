75ecf6e4d2629852c7a77f58673c63f21bf9a660
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 10:54:21 +0330

    Update 02_Even\Odd.cpp

--- 02_Even\Odd.cpp
+++ 02_Even\Odd.cpp
@@ -1,1 +1,15 @@
-
+void evenOddBetween() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    if (a > b) swap(a, b);
+    int choice;
+    cout << "Enter 1 for even numbers, 2 for odd numbers: ";
+    cin >> choice;
+    cout << "Numbers in range: ";
+    for (int i = a; i <= b; i++) {
+        if (choice == 1 && i % 2 == 0) cout << i << " ";
+        if (choice == 2 && i % 2 != 0) cout << i << " ";
+    }
+    cout << endl;
+}
