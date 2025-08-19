c3ccace414a42e49afabb82d55a9463500f439f0
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 11:05:19 +0330

    Update 13_Power_A^B.cpp

--- 13_Power_A^B.cpp
+++ 13_Power_A^B.cpp
@@ -1,1 +1,8 @@
-
+void powerAB() {
+    int a, b;
+    cout << "Enter a and b: ";
+    cin >> a >> b;
+    long long res = 1;
+    for (int i = 0; i < b; i++) res *= a;
+    cout << a << "^" << b << " = " << res << endl;
+}
