7ea57bd1ced05e16697920a9b3fbf1044a7af236
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 11:06:06 +0330

    Update 16_Max_and_Min_in_Array.cpp

--- 16_Max_and_Min_in_Array.cpp
+++ 16_Max_and_Min_in_Array.cpp
@@ -1,1 +1,14 @@
+void maxMinN() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[100];
+    for (int i = 0; i < n; i++) { cout << "Enter element " << i + 1 << ": "; cin >> arr[i]; }
+    int mn = arr[0], mx = arr[0];
+    for (int i = 1; i < n; i++) { if (arr[i] < mn) mn = arr[i]; if (arr[i] > mx) mx = arr[i]; }
+    cout << "Min: " << mn << ", Max: " << mx << endl;
+}
 
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
