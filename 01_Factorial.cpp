b3262b8e83900b330e14123761edec8b58e7072d
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 10:53:59 +0330

    Update 01_Factorial.cpp

--- 01_Factorial.cpp
+++ 01_Factorial.cpp
@@ -1,1 +1,9 @@
-
+long long factorial() {
+    int n;
+    cout << "Enter a number for factorial: ";
+    cin >> n;
+    long long result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    cout << "Factorial of " << n << " is " << result << endl;
+    return result;
+}
