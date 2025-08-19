e4d74fc31ae0f9560da16dd7fb2af6c06f8e19a9
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 11:11:12 +0330

    Update 10_Square_of_Stars.cpp

--- 10_Square_of_Stars.cpp
+++ 10_Square_of_Stars.cpp
@@ -1,1 +1,7 @@
-
+void squareStars() {
+    int n;
+    cout << "Enter size of square: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+for (int j = 0; j < n; j++) cout << "* "; cout << endl; }
+}
