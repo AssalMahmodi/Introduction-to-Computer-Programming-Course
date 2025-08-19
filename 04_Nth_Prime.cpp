5a81dfff04fa989f0d2be827000576720a1d64f3
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 10:55:08 +0330

    Update 04_Nth_Prime.cpp

--- 04_Nth_Prime.cpp
+++ 04_Nth_Prime.cpp
@@ -1,1 +1,17 @@
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i <= n / 2; i++) if (n % i == 0) return false;
+    return true;
+}
 
+void nthPrime() {
+    int n;
+    cout << "Enter n for nth prime: ";
+    cin >> n;
+    int count = 0, num = 1;
+    while (count < n) {
+        num++;
+        if (isPrime(num)) count++;
+    }
+    cout << "The " << n << "th prime number is " << num << endl;
+}
