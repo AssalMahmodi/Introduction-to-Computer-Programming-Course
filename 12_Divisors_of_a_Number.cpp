004e0f07bbd2b12d23a3cdd763bf725faa40a42d
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 11:05:03 +0330

    Update 12_Divisors_of_a_Number.cpp

--- 12_Divisors_of_a_Number.cpp
+++ 12_Divisors_of_a_Number.cpp
@@ -1,1 +1,8 @@
-
+void divisors() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Divisors: ";
+    for (int i = 1; i <= n; i++) if (n % i == 0) cout << i << " ";
+    cout << endl;
+}
