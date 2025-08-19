6547c785af0c30bd58441f73e61bccb932d6076f
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 11:39:31 +0330

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,1 +1,48 @@
+void showMenu() {
+    setColor(14); 
+    cout << "===============================" << endl;
+    setColor(9);
+    cout << "Welcome" << endl;
+    cout << "Principles of Computer Programming Project" << endl;
+    cout << "Seyedeh Asal Mahmodi Nezhad Dezfouli" << endl;
+    cout << "Professor: Dr. Saeed Ansari" << endl;
+    cout << "Summer 2025" << endl;
+    cout << "University of Guilan" << endl;
+    setColor(14);
+    cout << "===============================" << endl << endl;
 
+    setColor(14); 
+    cout << "============================" << endl;
+    setColor(13); 
+    cout << "        MAIN MENU           " << endl;
+    setColor(14);
+    cout << "============================" << endl;
+
+    setColor(10); 
+    cout << "0. "; setColor(7); cout << "Factorial" << endl;
+    setColor(10); cout << "1. "; setColor(7); cout << "Even/Odd Between" << endl;
+    setColor(10); cout << "2. "; setColor(7); cout << "Fibonacci" << endl;
+    setColor(10); cout << "3. "; setColor(7); cout << "Nth Prime" << endl;
+    setColor(10); cout << "4. "; setColor(7); cout << "Check Perfect Number" << endl;
+    setColor(10); cout << "5. "; setColor(7); cout << "Date Converter (Shamsi ↔ Gregorian)" << endl;
+    setColor(10); cout << "6. "; setColor(7); cout << "Top 3 Student Average" << endl;
+    setColor(10); cout << "7. "; setColor(7); cout << "GCD and LCM" << endl;
+    setColor(10); cout << "8. "; setColor(7); cout << "Sum and Product of Digits" << endl;
+    setColor(10); cout << "9. "; setColor(7); cout << "Count Words and Letters" << endl;
+    setColor(10); cout << "10. "; setColor(7); cout << "Square of Stars" << endl;
+    setColor(10); cout << "11. "; setColor(7); cout << "3-Digit Palindromes" << endl;
+    setColor(10); cout << "12. "; setColor(7); cout << "Divisors of a Number" << endl;
+    setColor(10); cout << "13. "; setColor(7); cout << "Power A^B" << endl;
+    setColor(10); cout << "14. "; setColor(7); cout << "Substring Search" << endl;
+    setColor(10); cout << "15. "; setColor(7); cout << "Bubble Sort" << endl;
+    setColor(10); cout << "16. "; setColor(7); cout << "Max and Min in Array" << endl;
+    setColor(10); cout << "17. "; setColor(7); cout << "Help (Function Descriptions)" << endl;
+
+
+    setColor(12); 
+    cout << "-1. "; cout << "Exit" << endl;
+
+    setColor(14);
+    cout << "============================" << endl;
+    setColor(7); 
+}
