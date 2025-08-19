e6b086ceaf61f88da647815b57fa9e161b12513a
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 11:05:50 +0330

    Update 15_Bubble_Sort.cpp

--- 15_Bubble_Sort.cpp
+++ 15_Bubble_Sort.cpp
@@ -1,1 +1,13 @@
-
+void bubbleSort() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[100];
+    for (int i = 0; i < n; i++) { cout << "Enter element " << i + 1 << ": "; cin >> arr[i]; }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) if (arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+    }
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << endl;
+}
