a47d94e138a9ebe4dec50ca2ab082e33a165eee4
Asal Mahmodi <147315309+AssalMahmodi@users.noreply.github.com>
2025-08-19 10:56:52 +0330

    Update 08_GCD_LCM.cpp

--- 08_GCD_LCM.cpp
+++ 08_GCD_LCM.cpp
@@ -1,1 +1,10 @@
-
+void gcdAndLcm() {
+    int a, b;
+    cout << "Enter two numbers for GCD and LCM: ";
+    cin >> a >> b;
+    int a_copy = a, b_copy = b;
+    while (b != 0) { int r = a % b; a = b; b = r; }
+    int gcd = a;
+    int lcm = (a_copy / gcd) * b_copy;
+    cout << "GCD: " << gcd << ", LCM: " << lcm << endl;
+}
